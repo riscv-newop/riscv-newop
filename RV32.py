85bd03d4761e04baebb5d048eda849c5a64905df
pry <pranoydll@gmail.com>
2020-05-30 17:01:59 -0500

    Created RV32 class

--- RV32.py
+++ RV32.py
@@ -1,3 +1,19 @@
-# General class for RISC-V 32bit
-class RV32:
-    pass
+from I32 import I32
+from RVFormatParser import RVFormatParser
+
+# TODO add support for M,A,C, etc
+# TODO accept
+class RV32(I32):
+    """ General class for RISC-V 32bit """
+
+    def __init__():
+        self.instructionTable = I32.instructionTable
+        self.program = {} # key is pc, value is RVInstruction
+
+    def addInstruction(self, pc, ba):
+        """ Adds Instruction from pc into program """
+        self.program[pc] = self.instructionTable[RVFormatParser.getOpcode(ba)](ba)
+
+    def printAll(self):
+        for pc in self.program:
+            print("{}: {}".format(pc, self.program[pc]))
