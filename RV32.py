fe2fefce40e20e9fabef10f73aa0aecfbbb9cc2d
pry <pranoydll@gmail.com>
2020-05-31 13:56:00 -0500

    Added ISA parameter to RV32
    This will allow the use of extensions along with the base Integer ISA.

--- RV32.py
+++ RV32.py
@@ -1,13 +1,22 @@
 from I32 import I32
+from M32 import M32
 from RVFormatParser import RVFormatParser
 
 
 # TODO add support for M,A,C, etc
-class RV32(I32):
+class RV32:
     """ General class for RISC-V 32bit """
 
-    def __init__(self):
-        self.instructionTable = I32.instructionTable
+    def __init__(self, isa="32I"):
+        """ A constructor for RV32
+            isa is a string containing which instruction sets and extensions to use, by default this will use 32I"""
+        self.instructionTable = {}
+
+        if "32I" in isa:
+            self.instructionTable.update(I32.instructionTable)
+        if "M" in isa:
+            self.instructionTable.update(M32.instructionTable)
+
         self.program = {}  # key is pc, value is RVInstruction
 
     def addInstruction(self, pc, ba):
