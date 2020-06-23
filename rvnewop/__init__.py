739c6ee439c03d272ac5e9553c0a83f7f3b3c83b
pry <pranoydll@gmail.com>
2020-06-23 16:34:49 -0500

    updated minor version

--- rvnewop/__init__.py
+++ rvnewop/__init__.py
@@ -1,5 +1,5 @@
 """ A RISC-V New Instruction Recommender System """
-__version__ = "0.1"
+__version__ = "0.1.0"
 
 from .RVFormatParser import RVFormatParser
 from .RVInstruction import RVInstruction
