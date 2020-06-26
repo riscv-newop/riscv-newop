b77ff2ebf0532a4665e85adafe6ecc410fd4a71a
pry <pranoydll@gmail.com>
2020-06-26 16:53:13 -0500

    updated basic test

--- tests/test_add.py
+++ tests/test_add.py
@@ -7,4 +7,4 @@ def test_add_example():
     rv = RV32("32I")
     binary = bitarray(bin(int("00e787b3", 16))[2:].zfill(32))
 
-    assert str(rv.decode(binary)) == "add x15 x15 x14"
+    assert str(rv.decode(binary)) == "add a5,a5,a4"
