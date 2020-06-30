5ba06bafce2cb75dd77b1db1be466f780b99e632
pry <pranoydll@gmail.com>
2020-06-30 15:12:02 -0500

    updated Program api

--- rvnewop/__init__.py
+++ rvnewop/__init__.py
@@ -1,5 +1,5 @@
 """ A RISC-V New Instruction Recommender System """
-__version__ = "0.5.2"
+__version__ = "0.5.3"
 
 from .RVFormatParser import RVFormatParser
 from .RVInstruction import RVInstruction
@@ -10,6 +10,7 @@ from .M32 import M32
 from .V32 import V32
 from .RV32 import RV32
 from .Histogram import Histogram
+from .Program import Program
 
 from .main import main
 from .unused import unused
