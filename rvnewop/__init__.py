1165cca001819a7b356d4e33e6218fa198d41704
pry <pranoydll@gmail.com>
2020-07-04 15:44:53 -0500

    bumped version for pypi package
    Forgot to update version in last commit before I pushed O_O

--- rvnewop/__init__.py
+++ rvnewop/__init__.py
@@ -1,5 +1,5 @@
 """ A RISC-V New Instruction Recommender System """
-__version__ = "0.5.11"
+__version__ = "0.6.0"
 
 from .RVFormatParser import RVFormatParser
 from .RVInstruction import RVInstruction
