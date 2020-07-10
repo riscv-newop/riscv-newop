c930b2441c49fb689e0e3da53f98ca355d76b03d
pry <pranoydll@gmail.com>
2020-07-10 18:55:35 -0500

    Bump version 0.7.4 fix #8

--- rvnewop/__init__.py
+++ rvnewop/__init__.py
@@ -1,5 +1,5 @@
 """ A RISC-V New Instruction Recommender System """
-__version__ = "0.7.3"
+__version__ = "0.7.4"
 
 from .RVFormatParser import RVFormatParser
 from .RVInstruction import RVInstruction
--- tests/test_with_dump_file.py
+++ tests/test_with_dump_file.py
@@ -5,13 +5,14 @@ from bitarray import bitarray
 from rvnewop import RV32
 from glob import glob
 
+
 class DumpFileReader:
     def __init__(self, file_name):
         rv = RV32("32IC")
         if os.path.exists(file_name) == False:
-            print ("Fatal: Unable to find the file: " + file_name)
+            print("Fatal: Unable to find the file: " + file_name)
         else:
-            df_handle = open(file_name, 'r')
+            df_handle = open(file_name, "r")
             lines = df_handle.readlines()
             for line in lines:
                 words = line.split()
@@ -19,19 +20,19 @@ class DumpFileReader:
                 if len(words) > 3:
                     # PC is recorded in the dump file with syntac <PC>:
                     # Strip the : away
-                    words[0]=words[0].replace(':','')
+                    words[0] = words[0].replace(":", "")
 
                     # Some false matches such as the line "Disassembly of section .text:"
                     # Remove those by ensuring that the first word is a valid hex value
                     if all(c in string.hexdigits for c in words[0]):
                         instruction_word = words[1]
                         decoded_str = rv.decodeHex(instruction_word)
-                        print ("Instruction at : " + str(words[0]))
+                        print("Instruction at : " + str(words[0]))
                         if decoded_str is None:
                             continue
                         decoded_instr = str(decoded_str).split()
                         assert decoded_instr[0] == words[2]
-                        
+
 
 def test_dump_file():
     files = glob(os.path.join(".", "*.dump"))
