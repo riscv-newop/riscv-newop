3bd3668654729b6a91608891d7262f0408a94a79
pry <pranoydll@gmail.com>
2020-07-10 19:28:11 -0500

    Fix #2
    Added objdump file testing

--- tests/test_dump_files.py
+++ tests/test_dump_files.py
@@ -1,12 +1,14 @@
-import os
-import pytest
 import string
-from bitarray import bitarray
-from rvnewop import RV32
+import os
 from glob import glob
+from rvnewop import RV32
+
+import pytest
 
 
 class DumpFileReader:
+    # TODO get rid of this __init__ and change to a
+    # seperate static method since this doesn't require state
     def __init__(self, file_name):
         rv = RV32("32IC")
         if os.path.exists(file_name) == False:
@@ -25,16 +27,29 @@ class DumpFileReader:
                     # Some false matches such as the line "Disassembly of section .text:"
                     # Remove those by ensuring that the first word is a valid hex value
                     if all(c in string.hexdigits for c in words[0]):
-                        instruction_word = words[1]
-                        decoded_str = rv.decodeHex(instruction_word)
-                        print("Instruction at : " + str(words[0]))
-                        if decoded_str is None:
-                            continue
-                        decoded_instr = str(decoded_str).split()
-                        assert decoded_instr[0] == words[2]
+
+                        inst_hex = words[1]
+                        inst_name = words[2]
+                        inst_params = words[3]
+
+                        predicted_inst = rv.decodeHex(inst_hex)
+                        predicted = str(predicted_inst)
+
+                        inst = "{} {}".format(inst_name, inst_params)
+
+                        # Check predicted instruction is correct
+                        assert predicted == inst
+
+                        print(
+                            "{} == {}".format(
+                                str(predicted_inst).split()[1], inst_params
+                            )
+                        )
 
 
 def test_dump_file():
-    files = glob(os.path.join(".", "*.dump"))
+    # TODO seperate this into multiple functions
+    # to see which ISAs failed
+    files = glob(os.path.join("tests", "*.dump"))
     for file in files:
         DumpFileReader(file)
