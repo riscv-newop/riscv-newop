56824d5f064b813a619180b5085daaa9ba5343de
pry <pranoydll@gmail.com>
2020-08-02 13:49:39 -0500

    Fix instructionTable key override bug
    I fixed this via function composition.

--- rvnewop/RV32.py
+++ rvnewop/RV32.py
@@ -10,6 +10,15 @@ from bitarray import bitarray
 class RV32:
     """ General class for RISC-V 32bit """
 
+    @staticmethod
+    def composition(f1, f2):
+        """Returns a composition of multiple instruction functions"""
+
+        def f3(ba):
+            return f1(ba) or f2(ba)
+
+        return f3
+
     def __init__(self, isa="32I"):
         """ A constructor for RV32
             isa is a string containing which instruction sets and extensions to use, by default this will use 32I"""
@@ -19,23 +28,38 @@ class RV32:
         self.instructionNameSet = set()
         self.registerSet = set()
 
+        toAdd = []
+
         if "32I" in isa:
-            self.instructionTable.update(I32.instructionTable)
+            # self.instructionTable.update(I32.instructionTable)
+            toAdd.append(I32.instructionTable)
             self.instructionNameSet.update(I32.instructionNameSet)
             self.registerSet.update(I32.registerSet)
         if "M" in isa:
-            self.instructionTable.update(M32.instructionTable)
+            # self.instructionTable.update(M32.instructionTable)
+            toAdd.append(M32.instructionTable)
             self.instructionNameSet.update(M32.instructionNameSet)
         if "V" in isa:
-            self.instructionTable.update(V32.instructionTable)
+            # self.instructionTable.update(V32.instructionTable)
+            toAdd.append(V32.instructionTable)
             # TODO add inst name set
             # TODO add register set
         if "C" in isa:
-            self.instructionTable.update(C32.instructionTable)
+            # self.instructionTable.update(C32.instructionTable)
+            toAdd.append(C32.instructionTable)
             self.instructionNameSet.update(C32.instructionNameSet)
 
             # TODO maybe use decorators or some other syntatic sugar
             # to automate this?
+
+        for table in toAdd:
+            for key in table:
+                if key in self.instructionTable:
+                    self.instructionTable[key] = self.composition(
+                        self.instructionTable[key], table[key]
+                    )
+                else:
+                    self.instructionTable[key] = table[key]
 
     def decodeHex(self, hex):
         """Decode an instruction encoded in hexadecimal
@@ -56,5 +80,5 @@ class RV32:
         """Decode an instruction encoded in binary as a bitarray
         Returns RVInstruction"""
         return self.instructionTable.get(
-            RVFormatParser.getOpcode(ba), lambda x:RVInstruction(rv_name="error")
+            RVFormatParser.getOpcode(ba), lambda x: RVInstruction(rv_name="error")
         )(ba)
