e2d44d653ae541f61f481badc2b799fdc690ba9b
Gulur <ndg0068@alveo-cse.unt.ad.unt.edu>
2020-09-06 18:00:34 -0500

    Update for liveness check

--- examples/graph.py
+++ examples/graph.py
@@ -7,22 +7,30 @@ from networkx.drawing.nx_agraph import graphviz_layout
 from tqdm import tqdm
 
 # FILENAME = "./examples/hst/pc_hist3.hst"
-FILENAME = "./embench_hst/aha-mont64.hst"
+# FILENAME = "./aha-mont64.hst"
+# FILENAME = "./wikisort.hst"
+FILENAME = "./matmult-int.hst"
 
 
 prog = rv.Histogram.parse(FILENAME, isa="32ICM")
 prog.findBasicBlocks()
+prog.addLivenessValuesToGraph()
+#prog.printSubBlocks()
 
+#sys.exit()
+
+orig = prog.getTotalInstructionCount()
 
 i = 0
+new = orig
 for b in tqdm(prog.getSubBlocks()):
     # for b in prog.getSubBlocks():
     graph = b.constructDAG()
 
-    for n in rv.analysis.findCandidateSubgraphs(graph):
+    for n in rv.analysis.findCandidateSubgraphs(prog, graph):
+        new -= 2*b.frequency
         plt.clf()
         sg = rv.analysis.createSubtreeFromNode(graph, n)
-        subgraph = rv.Subgraph(sg, n)
 
         # print(
         #     "{} hash: {} paren: {}".format(
@@ -34,6 +42,7 @@ for b in tqdm(prog.getSubBlocks()):
 
         pos = graphviz_layout(graph, prog="dot")
         nx.draw(sg, pos, with_labels=True, font_size=20)
-
-        plt.savefig("output{}_{}.png".format(i, subgraph.score))
+        plt.savefig("output" + str(i) + ".png")
         i += 1
+
+print ("Original instructions " + str(orig))
--- examples/score.py
+++ examples/score.py
@@ -11,42 +11,43 @@ import networkx as nx
 # from tqdm import tqdm
 from glob import glob
 
-# FILENAME = "./embench_hst/aha-mont64.hst"
+FILENAME = "./matmult-int.hst"
 
-for FILENAME in glob("./embench_hst/*.hst"):
-    print("Processing {}...".format(FILENAME))
+#for FILENAME in glob("./embench_hst/*.hst"):
+print("Processing {}...".format(FILENAME))
 
-    prog = rv.Histogram.parse(FILENAME, isa="32ICM")
-    prog.findBasicBlocks()
+prog = rv.Histogram.parse(FILENAME, isa="32ICM")
+prog.findBasicBlocks()
+prog.addLivenessValuesToGraph()
 
-    # total cycles = sum of all the frequencies
-    total_cycles = sum(
-        [inst.freq for bb in prog.basicBlocks for inst in bb.bbInstructions()]
+# total cycles = sum of all the frequencies
+total_cycles = sum(
+    [inst.freq for bb in prog.basicBlocks for inst in bb.bbInstructions()]
+)
+
+new_instructions = []
+
+for i, b in enumerate((prog.getSubBlocks())):
+    graph = b.constructDAG()
+
+    # temp will store every permutation of candidate subgraphs
+    temp = []
+    for n in rv.analysis.findCandidateSubgraphs(prog, graph):
+        subtree = rv.analysis.createSubtreeFromNode(graph, n)
+        root = n
+
+        temp.append(rv.Subgraph(subtree, root))
+
+    # add candidate subgraph with highest score to new instructions list
+    if temp:
+        temp = sorted(temp, key=lambda sg: sg.score, reverse=True)
+        new_instructions.append(temp[0])
+
+saved_cycles = sum([sg.score for sg in new_instructions])
+percent_cycles = float(saved_cycles) / total_cycles * 100
+
+print(
+    """Saved {} cycles out of {} ({:.4}%) with {} new instructions""".format(
+        saved_cycles, total_cycles, percent_cycles, len(new_instructions)
     )
-
-    new_instructions = []
-
-    for i, b in enumerate((prog.getSubBlocks())):
-        graph = b.constructDAG()
-
-        # temp will store every permutation of candidate subgraphs
-        temp = []
-        for n in rv.analysis.findCandidateSubgraphs(graph):
-            subtree = rv.analysis.createSubtreeFromNode(graph, n)
-            root = n
-
-            temp.append(rv.Subgraph(subtree, root))
-
-        # add candidate subgraph with highest score to new instructions list
-        if temp:
-            temp = sorted(temp, key=lambda sg: sg.score, reverse=True)
-            new_instructions.append(temp[0])
-
-    saved_cycles = sum([sg.score for sg in new_instructions])
-    percent_cycles = float(saved_cycles) / total_cycles * 100
-
-    print(
-        """Saved {} cycles out of {} ({:.4}%) with {} new instructions""".format(
-            saved_cycles, total_cycles, percent_cycles, len(new_instructions)
-        )
-    )
+)
